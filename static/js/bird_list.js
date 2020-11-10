063814150e411835b5aa27c8b222ec454bf17217
vagrant <vagrant@vagrant.vm>
2020-11-10 00:11:45 +0000

    added new html/js files

---
+++ static/js/bird_list.js
@@ -0,0 +1,12 @@
+// $('#list-adder').on('dblclick', () => {
+//     $('#list').append('<li>Item</li>');
+//     });
+
+// $('birding_list').append('<div>bird</div>')
+
+// $.get('/ebird-call', birds =>{
+    
+//     for(bird of birds){
+//         $('#birding_list').push('<div>{{bird["comName"]}}</div>')
+//     }
+//     });
---
+++ static/js/map.js
@@ -0,0 +1,36 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: { lat: 44.977753, lng: -93.2650108 },
+    });
+    const geocoder = new google.maps.Geocoder();
+    document.getElementById("submit").addEventListener("click", () => {
+      geocodeAddress(geocoder, map);
+    });
+  }
+  
+  function geocodeAddress(geocoder, resultsMap) {
+    const address = document.getElementById("address").value;
+    geocoder.geocode({ address: address }, (results, status) => {
+      if (status === "OK") {
+        resultsMap.setCenter(results[0].geometry.location);
+        new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location,
+        });
+        let lat = results[0].geometry.location.lat();
+        let lng = results[0].geometry.location.lng();
+        let lat_lng = {lat:lat, lng:lng}
+        console.log(lat);
+        console.log(lng);
+        $.post('/ebird-call', lat_lng, (res) =>{
+          alert(res);
+        
+      });}
+      else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+
+    });
+  }
+  
